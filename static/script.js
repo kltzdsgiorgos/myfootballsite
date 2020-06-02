6f8313fbe17c3d9624badd6c247e3dfe19d9966b
Georgios Kalaitzidis <giorgoskltzds@gmail.com>
2020-06-03 00:40:57 +0300

    proper link this time

--- static/script.js
+++ static/script.js
@@ -10,7 +10,7 @@ app.appendChild(logo)
 app.appendChild(container)
 
 var request = new XMLHttpRequest()
-request.open('GET', 'https://a-recoba.herokuapp.com//fixtures', true)
+request.open('GET', 'https://a-recoba.herokuapp.com/fixtures', true)
 request.onload = function() {
     // Begin accessing JSON data here
     var data = JSON.parse(this.response)
