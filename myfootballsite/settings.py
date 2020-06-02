bf050121e8a40d30554234378b10c0d64dc45f1c
Georgios Kalaitzidis <giorgoskltzds@gmail.com>
2020-06-02 22:53:45 +0300

    run colelct static

--- myfootballsite/settings.py
+++ myfootballsite/settings.py
@@ -124,21 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-print(STATIC_ROOT)
-print(STATICFILES_DIRS)
-print(STATIC_URL)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
 django_heroku.settings(locals())
-
