6848c2d9a83265051691f2bb045d4b0e6ca0d229
Georgios Kalaitzidis <giorgoskltzds@gmail.com>
2020-06-02 22:43:03 +0300

    testing

--- myfootballsite/settings.py
+++ myfootballsite/settings.py
@@ -15,7 +15,7 @@ import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -133,7 +133,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
+print(STATIC_ROOT)
+print(STATICFILES_DIRS)
+print(STATIC_URL)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
