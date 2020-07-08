cbffda5fcac657f6b10265e41b34a94e8c97132c
Georgios Kalaitzidis <giorgoskltzds@gmail.com>
2020-07-08 23:48:27 +0300

    changes 2

--- myfootballsite/settings.py
+++ myfootballsite/settings.py
@@ -24,7 +24,7 @@ print(BASE_DIR)
 SECRET_KEY = 'xd9rqe9=3f5&og@_q9dwv&a+((y-v7245f!eb*=(bo9lze7$fi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['mai-uom-19020.herokuapp.com']
 
--- script3.py
+++ script3.py
@@ -13,4 +13,4 @@ with open(CSV, newline='') as csvfile:
 	reader=csv.reader(csvfile, delimiter=',')
 	next(reader)
 	for row in reader:
-		EnglandFixtures.objects.create(hometeam=row[0],awayteam=row[1],homegoals=row[2],awaygoals=row[3])
+		EnglandFixtures.objects.create(season=row[0],hometeam=row[1],awayteam=row[2],homegoals=row[3],awaygoals=row[4])
